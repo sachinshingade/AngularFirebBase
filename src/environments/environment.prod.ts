b7e4516316db88fd3f967328f897f971bec750da
shai <shailesh.salekar652@gmail.com>
2018-09-01 21:28:06 +0530

    initial heroku deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:   {
+    apiKey: "AIzaSyC2P1UtsvR8SY2C-4SsMFZgIsoI24JL3uI",
+    authDomain: "auxesis-financial-services.firebaseapp.com",
+    databaseURL: "https://auxesis-financial-services.firebaseio.com",
+    projectId: "auxesis-financial-services",
+    storageBucket: "auxesis-financial-services.appspot.com",
+    messagingSenderId: "162104209831"
+  }
 };
