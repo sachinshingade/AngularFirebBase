0c0e32c7c8f83671c85c3c11d18d52d7b223ae5b
shai <shailesh.salekar652@gmail.com>
2018-09-01 19:15:51 +0530

    append

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/sbadmin'));
+app.use(express.static('./dist'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname,'/dist/sbadmin/index.html'));
+res.sendFile(path.join(__dirname,'/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8081);
