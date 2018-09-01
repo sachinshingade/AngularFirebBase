9bf75974f12ef9a2e967c1e56b89d80cf67a9121
shai <shailesh.salekar652@gmail.com>
2018-09-01 21:32:41 +0530

    initial heroku deploy

--- src/app/layout/charts/charts.component.ts
+++ src/app/layout/charts/charts.component.ts
@@ -148,14 +148,14 @@ export class ChartsComponent implements OnInit {
     // public doughnutChartData: number[] = [350, 450, 100];
     // public doughnutChartType: string = 'doughnut';
 
-    // // events
-    // public chartClicked(e: any): void {
-    //     // console.log(e);
-    // }
+    // events
+    public chartClicked(e: any): void {
+        // console.log(e);
+    }
 
-    // public chartHovered(e: any): void {
-    //     // console.log(e);
-    // }
+    public chartHovered(e: any): void {
+        // console.log(e);
+    }
 
 
 }
